6017c2a69f2777d5c763269bac4cd6e56c924a98
Mauro Juliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-09 13:25:48 -0400

    Initial Commit

---
+++ Simple-TabBar/Simple_TabBar.h
@@ -0,0 +1,18 @@
+//
+//  Simple_TabBar.h
+//  Simple-TabBar
+//
+//  Created by user195713 on 7/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Simple_TabBar.
+FOUNDATION_EXPORT double Simple_TabBarVersionNumber;
+
+//! Project version string for Simple_TabBar.
+FOUNDATION_EXPORT const unsigned char Simple_TabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Simple_TabBar/PublicHeader.h>
+
+
