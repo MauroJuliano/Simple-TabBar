8634e70f93b465b61a0b865f0e7d632bc83e8fc3
Mauro Juliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-09 13:59:23 -0400

    adicionando ponto, e bordar arredondada na tab bar

---
+++ Simple-TabBar/Classes/BorderTabBarController.swift
@@ -0,0 +1,93 @@
+//
+//  CustomTabBarController.swift
+//  Custom-Tabbar
+//
+//  Created by Mauro Figueiredo on 07/07/21.
+//  Copyright © 2021 Mauro Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+open class BorderTabBarController: UITabBarController {
+    fileprivate var shouldSelectOnTabBar = true
+    
+    open override var selectedViewController: UIViewController? {
+        willSet {
+            guard shouldSelectOnTabBar,
+                let newValue = newValue else {
+                    shouldSelectOnTabBar = true
+                    return
+            }
+            guard let tabBar = tabBar as? CustomTabBar, let index = viewControllers?.index(of: newValue) else {
+                return
+            }
+            tabBar.select(itemAt: index, animated: false)
+        }
+        }
+    open override var selectedIndex: Int {
+        willSet{
+            guard shouldSelectOnTabBar else {
+                shouldSelectOnTabBar = true
+                return
+            }
+            guard let tabBar = tabBar as? CustomTabBar else {
+                return
+            }
+            tabBar.select(itemAt: selectedIndex, animated: false)
+        }
+    }
+    open override func viewDidLoad(){
+        super.viewDidLoad()
+        let tabBar = CustomTabBar()
+        self.setValue(tabBar, forKey: "tabBar")
+    }
+    
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private var _barHeight: CGFloat = 74
+    open var barHeight: CGFloat {
+        get {
+            if #available(iOS 11.0, *) {
+                return _barHeight + view.safeAreaInsets.bottom
+            }else {
+                return _barHeight
+            }
+     }
+        set {
+            _barHeight = newValue
+            updateTabBarFrame()
+        }
+    }
+    
+    private func updateTabBarFrame(){
+        var tabFrame = self.tabBar.frame
+        tabFrame.size.height = barHeight
+        tabFrame.origin.y = self.view.frame.size.height - barHeight
+        self.tabBar.frame = tabFrame
+        tabBar.setNeedsLayout()
+    }
+    
+    open override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateTabBarFrame()
+    }
+    
+    open override func viewSafeAreaInsetsDidChange() {
+        if #available(iOS 11.0, *) {
+            super.viewSafeAreaInsetsDidChange()
+        }
+        updateTabBarFrame()
+    }
+    open override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let idx = tabBar.items?.index(of: item) else {
+            return
+        }
+        if let controller = viewControllers?[idx] {
+            shouldSelectOnTabBar = false
+            selectedIndex = idx
+            delegate?.tabBarController?(self, didSelect: controller)
+        }
+    }
+}
---
+++ Simple-TabBar/Classes/CBTabBarButton.swift
@@ -0,0 +1,201 @@
+//
+//  CBTabBarButton.swift
+//  Custom-Tabbar
+//
+//  Created by Mauro Figueiredo on 07/07/21.
+//  Copyright © 2021 Mauro Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+public class CBTabBarItem: UITabBarItem{
+    @IBInspectable public var tintColor: UIColor?
+    @IBInspectable public var rightToLeft:Bool = false
+}
+public class CBTabBarButton: UIControl {
+    
+    var dotView = UIView()
+    var rightToLeft:Bool = false
+    private var _isSelected: Bool = false
+    override public var isSelected: Bool {
+        get {
+            return _isSelected
+        }
+        set {
+            guard newValue != _isSelected else {
+                return
+            }
+            setSelected(newValue)
+        }
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureSubviews()
+    }
+    
+    init(item: UITabBarItem) {
+        super.init(frame: .zero)
+        tabImage = UIImageView(image: item.image)
+        defer {
+            self.item = item
+            configureSubviews()
+        }
+    }
+    
+    private var currentImage: UIImage? {
+        var maybeImage: UIImage?
+        if _isSelected {
+            maybeImage = item?.selectedImage ?? item?.image
+         } else {
+            maybeImage = item?.image
+         }
+        guard let image = maybeImage else {
+            return nil
+        }
+        return image.renderingMode == .automatic ? image.withRenderingMode(.alwaysTemplate) : image
+    }
+    
+    public var item: UITabBarItem? {
+        didSet {
+            tabImage.image = currentImage
+            tabLabel.text = item?.title
+            if let tabItem = item as? CBTabBarItem {
+                if let color = tabItem.tintColor {
+                    tintColor = color
+                }
+                rightToLeft = tabItem.rightToLeft
+            }
+        }
+    }
+    
+    override public var tintColor: UIColor! {
+        didSet {
+            if _isSelected {
+                tabImage.tintColor = tintColor
+            }
+            dotView.backgroundColor = tintColor
+            tabLabel.textColor = tintColor
+            tabBg.backgroundColor = tintColor.withAlphaComponent(0.2)
+        }
+    }
+    
+    private var tabImage = UIImageView()
+    private var tabLabel = UILabel()
+    private var tabBg = UIView()
+    
+    private let bgHeight: CGFloat = 42.0
+    private var csFoldedBgTrailing: NSLayoutConstraint!
+    private var csUnFoldedBgTrailing: NSLayoutConstraint!
+    private var csFoldedLBLLeading: NSLayoutConstraint!
+    private var csUnFoldedLBLLeading: NSLayoutConstraint!
+    
+    private var foldedConstraints: [NSLayoutConstraint] {
+        return [csFoldedLBLLeading, csFoldedBgTrailing]
+    }
+    private var unfoldedConstraints: [NSLayoutConstraint] {
+        return [csUnFoldedBgTrailing, csUnFoldedLBLLeading]
+    }
+    
+    private func configureSubviews(){
+        tabImage.contentMode = .center
+        tabImage.translatesAutoresizingMaskIntoConstraints = false
+        tabLabel.translatesAutoresizingMaskIntoConstraints = false
+        tabLabel.font = UIFont.systemFont(ofSize: 14)
+        tabLabel.adjustsFontSizeToFitWidth = true
+        tabBg.translatesAutoresizingMaskIntoConstraints = false
+        tabBg.isUserInteractionEnabled = false
+        tabImage.setContentHuggingPriority(.required, for: .horizontal)
+        tabImage.setContentHuggingPriority(.required, for: .vertical)
+        tabImage.setContentCompressionResistancePriority(.required, for: .horizontal)
+        tabImage.setContentCompressionResistancePriority(.required, for: .vertical)
+        
+        let dotSize: CGFloat = 5.0
+        dotView.frame = CGRect(origin: .zero, size: CGSize(width: dotSize, height: dotSize))
+        dotView.layer.cornerRadius = dotSize / 2.0
+        dotView.layer.shouldRasterize = true
+        dotView.layer.rasterizationScale = UIScreen.main.scale
+        dotView.isHidden = true
+        
+        self.addSubview(tabBg)
+        self.addSubview(tabLabel)
+        self.addSubview(tabImage)
+        self.addSubview(dotView)
+        
+        tabBg.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        tabBg.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        tabBg.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        tabBg.heightAnchor.constraint(equalToConstant: bgHeight).isActive = true
+        
+        if rightToLeft {
+            tabImage.trailingAnchor.constraint(equalTo: tabBg.trailingAnchor, constant: -bgHeight/2.0).isActive = true
+            tabImage.centerYAnchor.constraint(equalTo: tabBg.centerYAnchor).isActive = true
+            tabLabel.centerYAnchor.constraint(equalTo: tabBg.centerYAnchor).isActive = true
+            csFoldedLBLLeading = tabLabel.leadingAnchor.constraint(equalTo: tabBg.trailingAnchor)
+            csUnFoldedLBLLeading = tabLabel.leadingAnchor.constraint(equalTo: tabBg.leadingAnchor, constant: bgHeight/4.0)
+            csFoldedBgTrailing = tabImage.trailingAnchor.constraint(equalTo: tabBg.leadingAnchor, constant: bgHeight/2.0)
+            csUnFoldedBgTrailing = tabLabel.trailingAnchor.constraint(equalTo: tabImage.leadingAnchor, constant: -bgHeight/2.0)
+        } else {
+            tabImage.leadingAnchor.constraint(equalTo: tabBg.leadingAnchor, constant: bgHeight/2.0).isActive = true
+            tabImage.centerYAnchor.constraint(equalTo: tabBg.centerYAnchor).isActive = true
+            tabLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+            csFoldedLBLLeading = tabLabel.leadingAnchor.constraint(equalTo: leadingAnchor)
+            csUnFoldedLBLLeading = tabLabel.leadingAnchor.constraint(equalTo: tabImage.trailingAnchor, constant: bgHeight/4.0)
+            csFoldedBgTrailing = tabImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -bgHeight/2.0)
+            csUnFoldedBgTrailing = tabLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -bgHeight/2.0)
+            
+        }
+        fold()
+        setNeedsLayout()
+    }
+    
+    private func fold(animationDuration duration: Double = 0.0) {
+        unfoldedConstraints.forEach{$0.isActive = false}
+        foldedConstraints.forEach{ $0.isActive = true}
+        UIView.animate(withDuration: duration){
+            self.tabBg.alpha = 0.0
+        }
+        UIView.animate(withDuration: duration * 0.4) {
+            self.tabLabel.alpha = 0.0
+        }
+        UIView.transition(with: tabImage, duration: duration, options: [.transitionCrossDissolve], animations: {
+            self.tabImage.tintColor = .black
+        }, completion: nil)
+ }
+    private func unFold(animationDuration duration: Double = 0.0) {
+        unfoldedConstraints.forEach{$0.isActive = true}
+        foldedConstraints.forEach{ $0.isActive = false}
+        
+        UIView.animate(withDuration: duration){
+            self.tabBg.alpha = 1.0
+        }
+        UIView.animate(withDuration: duration * 0.5, delay: duration * 0.5, options: [], animations: {
+            self.dotView.isHidden = false
+            self.tabLabel.alpha = 1.0
+        }, completion: nil)
+        
+        UIView.transition(with: tabImage, duration: duration, options: [.transitionCrossDissolve], animations: {
+            self.tabImage.tintColor = self.tintColor
+        }, completion: nil)
+ }
+    
+    public func setSelected(_ selected: Bool, animationDuration duration: Double = 0.0){
+        _isSelected = selected
+        UIView.transition(with: tabImage, duration: 0.05, options: [.beginFromCurrentState], animations: {
+            self.tabImage.image = self.currentImage
+        }, completion: nil)
+        if selected {
+            unFold(animationDuration: duration)
+        }else {
+            fold(animationDuration: duration)
+        }
+    }
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        tabBg.layer.cornerRadius = tabBg.bounds.height / 2.0
+    }
+}
